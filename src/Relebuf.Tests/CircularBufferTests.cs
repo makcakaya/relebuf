2d9f12e3c8dd11db1d567e380c837874d74cba72
Mert Akcakaya <mert@biltera.com.tr>
2016-10-02 16:50:37 +0300

    Just doesn't make sense
    A circular queue type buffer is implemented and tested.

---
+++ src/Relebuf.Tests/CircularBufferTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Relebuf.Tests
+{
+    public sealed class CircularBufferTests
+    {
+        [Fact]
+        public void CanBuildValid()
+        {
+            new CircularBuffer<int>(100);
+        }
+
+        [Fact]
+        public void BuildInvalidThrows()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new CircularBuffer<int>(0));
+        }
+
+        [Fact]
+        public void InsertDoesNotOverwriteUnderCapacity()
+        {
+            uint capacity = 100;
+            var buffer = new CircularBuffer<int>(capacity);
+            for (var i = 0; i < capacity; i++)
+            {
+                buffer.Insert(i);
+            }
+
+            Assert.Equal(0, buffer.First());
+        }
+
+        [Fact]
+        public void InsertOverwritesAboveCapacity()
+        {
+            uint capacity = 100;
+            var buffer = new CircularBuffer<int>(capacity);
+            for (var i = 0; i < capacity + 10; i++)
+            {
+                buffer.Insert(i);
+            }
+
+            Assert.Equal(100, buffer.ElementAt(0));
+            Assert.Equal(101, buffer.ElementAt(1));
+        }
+
+        [Fact]
+        public void DoesNotThrowWhenOverwritesMultipleTimes()
+        {
+            uint capacity = 100;
+            var loopCount = 1000;
+            var totalInserts = capacity * loopCount;
+
+            var buffer = new CircularBuffer<int>(capacity);
+
+            for (var i = 0; i < totalInserts; i++)
+            {
+                buffer.Insert(i);
+            }
+
+            Assert.Equal(totalInserts - 1, buffer.ElementAt((int)capacity - 1));
+        }
+
+        [Fact]
+        public void ElementAtThrowsIfBeyondCapacity()
+        {
+            var buffalo = new CircularBuffer<int>(5);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => buffalo.ElementAt(5));
+        }
+    }
+}
---
+++ src/Relebuf/CircularBuffer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Relebuf
+{
+    public sealed class CircularBuffer<T> : IEnumerable<T>
+    {
+        private readonly uint _capacity;
+        private T[] _records = new T[0];
+        private uint _cursor = 0;
+
+        public CircularBuffer(uint capacity)
+        {
+            if (capacity == 0) { throw new ArgumentOutOfRangeException(nameof(capacity)); }
+
+            _capacity = capacity;
+        }
+
+        public void Insert(T item)
+        {
+            if (_cursor == _capacity)
+            {
+                _cursor = 0;
+            }
+            else
+            {
+                EnsureSize(_cursor + 1);
+            }
+
+            _records[_cursor] = item;
+            _cursor++;
+        }
+
+        private void EnsureSize(uint size)
+        {
+            if (_records.Length >= size)
+            {
+                return;
+            }
+
+            if (size > _capacity)
+            {
+                size = _capacity;
+            }
+            else
+            {
+                size *= 2;
+            }
+
+            // Create a bigger array and copy current records to it.
+            var newRecords = new T[size];
+            Array.Copy(_records, newRecords, _records.Length);
+
+            // Register new array.
+            _records = newRecords;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_records).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _records.GetEnumerator();
+        }
+    }
+}
